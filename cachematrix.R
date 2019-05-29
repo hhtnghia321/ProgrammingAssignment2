7c4566b7fcd0e88369d428297ffd856afcf3a7ac
hhtnghia321 <38842210+hhtnghia321@users.noreply.github.com>
2019-05-29 18:50:50 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,14 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
-  set <- function(y){
+  set <- function(y){         ##define the set matrix function
     x <<- y
     i <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inversedmatrix) i <<- inversedmatrix
-  getinverse <- function() i
-  list( set = set, 
+  get <- function() x ##define the get matrix function
+  setinverse <- function(inversedmatrix) i <<- inversedmatrix       ##define the function to contain the inversed matrix function
+  getinverse <- function() i                           ##define the function to get the inversed matrix function
+  list( set = set,                ## print out output in list data class
         get = get,
         setinverse = setinverse,
         getinverse = getinverse)
@@ -22,14 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  a <- x$getinverse()
-  if(!is.null(a) == TRUE) {
+  a <- x$getinverse()           ## get the inversed matrix from above function in the set function
+  if(!is.null(a) == TRUE) {       ## whether or not the inversed matrix already exist
     message("getting Inversed Matrix from data")
     return(a)
   } else {
-  getmtr <- x$get()
+  getmtr <- x$get()     ## if it not pre-existed , we will compute the inverse matrix by solve() function
   a <- solve(getmtr,...) 
-  x$setinverse(a)
+  x$setinverse(a)         ## define the input of setinverse by above result
   return(a)     ## Return a matrix that is the inverse of 'x'
   }
 }
